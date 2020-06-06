d7fd2419149ca207427c1933816169097e41c96f
Bruno Li <lisiuloong@gmail.com>
2020-06-06 12:29:23 -0400

    add a form to add a new project title

--- src/app.js
+++ src/app.js
@@ -5,42 +5,52 @@ import './vendor';
 
 const controller = ((UICtrl, todoCtrl) => {
 	const setupEventListeners = () => {
-		let DOMstr, addBtn, tasksContainer, todoContainer, modalContainer, closeModalBtn;
+		let DOMstr, addProjectBtn, addListBtn, listTitle, projectModal;
+
 		DOMstr = UICtrl.getDom();
-		addBtn = document.querySelector(DOMstr.addBtnTask);
-		tasksContainer = document.querySelector(DOMstr.tasksContainer);
-		todoContainer = document.querySelector(DOMstr.todoContainer);
-		modalContainer = document.querySelector(DOMstr.modal);
-		closeModalBtn = document.getElementById(DOMstr.closeModal);
+		// elements
+		addProjectBtn = document.querySelector(DOMstr.addProjectBtn);
+		projectModal = document.querySelector(DOMstr.projectModal);
+		addListBtn = document.querySelector(DOMstr.addListBtn);
+		listTitle = document.querySelector(DOMstr.listTitle);
 
-		addBtn.addEventListener('click', addTask);
+		// tasksContainer = document.querySelector(DOMstr.tasksContainer);
+		// todoContainer = document.querySelector(DOMstr.todoContainer);
+		// modalContainer = document.querySelector(DOMstr.modal);
 
-		todoContainer.addEventListener('click', (event) => {
-			if (event.target.classList.contains('project')) {
-				toggleModal(DOMstr);
-			} else if (event.target.classList.contains('close-button')) {
-				toggleModal(DOMstr);
-			}
+		// closeModalBtn = document.getElementById(DOMstr.closeModal);
+
+		addProjectBtn.addEventListener('click', () => {
+			projectModal.classList.toggle('show-modal');
 		});
+
+		addListBtn.addEventListener('click', (e) => {
+			e.preventDefault();
+			UICtrl.addProject(listTitle.value);
+			// listTitle.value = '';
+			// console.log(listTitle.value);
+			projectModal.classList.toggle('show-modal');
+		});
+
+		// todoContainer.addEventListener('click', (event) => {
+		// 	if (event.target.classList.contains('project')) {
+		// 		toggleModal(DOMstr);
+		// 	} else if (event.target.classList.contains('close-button')) {
+		// 		toggleModal(DOMstr);
+		// 	}
+		// });
 
 		window.addEventListener('click', (event) => {
-			if (event.target.classList.contains('modal')) {
-				toggleModal(DOMstr);
+			if (event.target.classList.contains('modal__project')) {
+				projectModal.classList.toggle('show-modal');
+			} else if (event.target.classList.contains('close-button')) {
+				projectModal.classList.toggle('show-modal');
 			}
 		});
 	};
 
-	const toggleModal = (DOMstr) => {
-		document.querySelector(DOMstr.modal).classList.toggle('show-modal');
-	};
-
-	const addTask = () => {
-		UICtrl.displayNewTask();
-		// console.log(event);
-	};
-
-	UICtrl.test();
-	todoCtrl.test();
+	// UICtrl.test();
+	// todoCtrl.test();
 
 	return {
 		init: () => {
--- src/ui-controller.js
+++ src/ui-controller.js
@@ -1,36 +1,36 @@
 export const UIController = (() => {
 	let DOMStrings = {
+		projectModal: '.modal__project',
+		addProjectBtn: '.add__btn',
+		addListBtn: '.add__list-btn',
+		listTitle: '.project__name',
 		todoContainer: '.todo__container',
 		closeBtn: '.close-button',
 		tasksContainer: '.tasks__container',
-		addBtnTask: '.add__btn',
-		modal: '.modal',
 		closeModal: '.close-button'
 	};
 
 	return {
-		displayNewTask: () => {
+		addProject: (title) => {
+			console.log(title);
 			let newTask = `<div class="tasks__container">
                     <div class="task">
-                        <h2 class="project">%Project%</h2>
-                        <div class="modal">
-                            <div class="modal-content">
+                        <h2 class="project">${title}</h2>
+                        <div class="modal__project">
+                            <div class="modal__project-content">
                                 <span class="close-button">&times;</span>
-                                <h3>%project%</h3>
+                                <h3>${title}</h3>
                                 <ul class="project__list">
-                                    <li>1</li>
-                                    <li>2</li>
-                                    <li>3</li>
-                                    <li>4</li>
-                                    <li>5</li>
+                                    <li>Lorem ipsum dolor sit amet.</li>
                                 </ul>
-                                 <span class="close-button">&times;</span>
+                                <span class="close-button">&times;</span>
                             </div>
                         </div>
                     </div>
                 </div>`;
 			document.querySelector(DOMStrings.todoContainer).insertAdjacentHTML('beforeend', newTask);
 		},
+
 		getDom: () => DOMStrings,
 		test: () => console.log('UIController')
 	};
